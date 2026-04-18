72f6da958af3f27be2984521231d3b29c6df210c
MiguelTraversaro <70926143+MiguelTraversaro@users.noreply.github.com>
2026-04-18 16:34:39 -0300

    backendApplication

---
+++ BackEnd-Java-Spring/src/main/java/com/gymApp/backend/BackendApplication.java
@@ -0,0 +1,8 @@
+package com.gymApp.backend;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class BackendApplication {
+    public BackendApplication() { /* compiled code */ }
+
+    public static void main(java.lang.String[] args) { /* compiled code */ }
+}
